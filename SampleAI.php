6e4bb88c9b2eb28000d7d86a70810ba4ace9621b
dekokun <shintaro.kurachi@gmail.com>
2014-08-10 21:15:58 +0900

    IOをクラス操作にまとめた

--- SampleAI.php
+++ SampleAI.php
@@ -1,9 +1,20 @@
 <?php
 
-class Hoge {
+class StdIO {
     protected $fp;
-    public function getStdIn() {
-        return explode(' ', rtrim(fgets($this->fp)));
+
+    public function __construct($fp) {
+        $this->fp = $fp;
+    }
+
+    public function getIn() {
+        return fgets($this->fp);
+    }
+    public function outPut($data) {
+        echo $data;
+    }
+    public function outPutArray(array $array) {
+        $this->outPut(implode(' ', $array) . PHP_EOL);
     }
 }
 
@@ -21,7 +32,10 @@ logging('end');
 
 class Game {
 
-    protected $fp;
+    /**
+     * @var StdIO
+     */
+    protected $io;
     protected $maxTurn;
     protected $numOfPlayers;
     protected $numOfHeroines;
@@ -31,11 +45,11 @@ class Game {
 
     public function __construct() {
         logging('hogehoge');
-        $this->fp = fopen('php://stdin', 'r');
+        $this->io = new StdIO(fopen('php://stdin', 'r'));
     }
 
     public function main() {
-        echo 'READY';
+        $this->io->outPut('READY');
         $this->readGameSetting();
 
         for ($i = 0; $i < $this->maxTurn; $i++) {
@@ -46,28 +60,28 @@ class Game {
     }
 
     function readGameSetting() {
-        $gameSettings = explode(' ', rtrim(fgets($this->fp)));
+        $gameSettings = explode(' ', rtrim($this->io->getIn()));
         $this->maxTurn = $gameSettings[0];
         $this->numOfPlayers = $gameSettings[1];
         $this->numOfHeroines = $gameSettings[2];
 
-        $gameSettings = explode(' ', rtrim(fgets($this->fp)));
+        $gameSettings = explode(' ', rtrim($this->io->getIn()));
         foreach ($gameSettings as $enthusiasm) {
             array_push($this->heroines, new Heroine((integer)$enthusiasm));
         }
     }
 
     function readData() {
-        list($turn, $this->day) = explode(' ', rtrim(fgets($this->fp)));
+        list($turn, $this->day) = explode(' ', rtrim($this->io->getIn()));
         for ($i = 0; $i < $this->numOfHeroines; $i++) {
-            $revealedScores = explode(' ', rtrim(fgets($this->fp)));
+            $revealedScores = explode(' ', rtrim($this->io->getIn()));
         }
-        $realScores = explode(' ', rtrim(fgets($this->fp)));
+        $realScores = explode(' ', rtrim($this->io->getIn()));
         for ($i = 0; $i < $this->numOfHeroines; $i++) {
             $this->heroines[$i]->setRealScore((integer)$realScores[$i]);
         }
         if ($this->day === 'W') {
-            $dated = explode(' ', rtrim(fgets($this->fp)));
+            $dated = explode(' ', rtrim($this->io->getIn()));
             for ($i = 0; $i < $this->numOfHeroines; $i++) {
                 $this->heroines[$i]->setDated((integer)$dated[$i]);
             }
@@ -75,20 +89,15 @@ class Game {
     }
 
     function writeCommand() {
-        $command = '';
+        $heroineNums = [];
         if ($this->day === 'W') {
             for ($i = 0; $i < 5; $i++) {
-                $command = $command . mt_rand(0, $this->numOfHeroines - 3);
-                if($i < 4) {
-                    $command = $command . ' ';
-                }
+                $heroineNums[] = mt_rand(0, $this->numOfHeroines - 3);
             }
         } else {
-            $command = $command . mt_rand(0, $this->numOfHeroines - 1) . ' ' . mt_rand(0, $this->numOfHeroines - 3);
+            $heroineNums[] = mt_rand(0, $this->numOfHeroines - 1) . ' ' . mt_rand(0, $this->numOfHeroines - 3);
         }
-
-        $command = $command . PHP_EOL;
-        echo $command;
+        $this->io->outPutArray($heroineNums);
     }
 
 }
