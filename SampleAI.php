447f07b61cc4c3827459288a61fd39c96d1f06d3
dekokun <shintaro.kurachi@gmail.com>
2014-08-13 09:17:49 +0900

    loggingの際は全て文字列表現になおして出力
    これでfalseなども出力可能になった

--- SampleAI.php
+++ SampleAI.php
@@ -84,10 +84,7 @@ class GameSettings {
 }
 
 function logging($message) {
-    if (is_array($message)) {
-        $message = print_r($message, true);
-    }
-    $message = $message . PHP_EOL;
+    $message = var_export($message, true) . PHP_EOL;
     fputs(STDERR,$message);
     error_log($message, 3, '/tmp/hoge.log');
 }
