0deeda8e5bbb0b6c94347ed572aed787f6dc3396
dekokun <shintaro.kurachi@gmail.com>
2014-08-17 15:38:18 +0900

    テストを充実

--- test.php
+++ test.php
@@ -63,5 +63,11 @@ class AllTest extends PHPUnit_Framework_TestCase
         $this->assertEquals([40, 45, 45, 45], array_map(function($val) {return array_sum($val);}, $rule->allRemainPoints(new \Turn(1), new Heroines([0,0,0,0,0,0,0,0]))));
         $this->assertEquals([36, 40, 40, 40], array_map(function($val) {return array_sum($val);}, $rule->allRemainPoints(new \Turn(2), new Heroines([0,0,0,0,0,0,0,0]))));
         $this->assertEquals([0, 4, 4, 4], array_map(function($val) {return array_sum($val);}, $rule->allRemainPoints(new \Turn(10), new Heroines([0,0,0,0,0,0,0,0]))));
+        foreach($rule->allRemainPoints(new \Turn(10), new Heroines([0,0,0,0,0,0,0,0])) as $points) {
+            foreach($points as $point) {
+                $this->assertGreaterThanOrEqual(0, $point);
+            }
+        }
+        $this->assertNotEquals($rule->allRemainPoints(new \Turn(10), new Heroines([0,0,0,0,0,0,0,0])), $rule->allRemainPoints(new \Turn(10), new Heroines([0,0,0,0,0,0,0,0])), 'こちら、ごくごく稀に落ちるので注意');
     }
 }
